0847bc379642329a219b90416f0424635f366443
root <root@ubuntu-cloudpanel.localhost>
2024-05-13 07:35:04 +0000

    adding changes

--- school_feeding/school_feeding/settings.py
+++ school_feeding/school_feeding/settings.py
@@ -7,7 +7,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 
 SECRET_KEY = env("SECRET_KEY")
 
-DEBUG = True 
+DEBUG = False
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -135,19 +135,21 @@ FILE_UPLOAD_MAX_SIZE = 1000000000
 
 
 
-#STATIC_URL = "/static/"
-#STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "static/"
 
-STATIC_ROOT = ''
+STATIC_ROOT = BASE_DIR / "static"
 
-STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ('static',)
 
 MEDIA_URL = "media/"
+
 MEDIA_ROOT = BASE_DIR / "media"
 
-#STATICFILES_DIRS = [BASE_DIR / "school/build/static"]
+
+
+STATICFILES_DIRS = [BASE_DIR / "school/build/static"]
+
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CSRF_TRUSTED_ORIGINS = ['https://aroxj_aprelakerpi_despan.schoolfeeding.am']
