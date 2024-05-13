94f0552a3f8f9bbe3efb1ca7505a2c9ce2b0b410
RobQocharyan <robqocharyan@mail.ru>
2024-05-13 23:33:06 +0400

    f

--- school_feeding/school/src/App.js
+++ school_feeding/school/src/App.js
@@ -33,7 +33,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/forgetpass" element={<ForgetPass />} />
           <Route path="/newpass" element={<NewPass />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/register" element={<Register />} /> 
           <Route path="touch" element={<Touch />} />
         </Routes>
       </div>
